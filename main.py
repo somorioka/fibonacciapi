ffc1d2f7726d9a9a6d9c91be35a9de7b8e245822
morioka <sou.kokomirai@gmail.com>
2023-02-13 16:48:44 +0900

    init_comit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+
+app = FastAPI()
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return JSONResponse(
+        {
+            "status":"400",
+            "message": "Bad request"
+        },
+        status_code=400
+        )
+
+@app.get("/fib/")
+async def fib(n: int):
+    if n <= 0:
+        return JSONResponse(
+            {
+                "status":"400",
+                "message": "1以上の整数を入力してください"
+            },
+            status_code=400
+            )
+
+    a, b = 1, 0
+    for _ in range(n):
+        a, b = a + b, a
+    return {"result": b}
---
+++ tests/mypkg/test_main.py
@@ -0,0 +1,26 @@
+from fastapi.testclient import TestClient
+
+from ...main import app
+
+client = TestClient(app)
+
+def test_read_number():
+    response = client.get("/fib?n=99")
+    assert response.status_code == 200
+    assert response.json() == {"result": 218922995834555169026}
+
+def test_read_bad_number():
+    response = client.get("/fib?n=-3")
+    assert response.status_code == 400
+    assert response.json() == {
+        "status":"400",
+        "message": "1以上の整数を入力してください"
+        }
+
+def test_read_bad_request():
+    response = client.get("/fib?n=a")
+    assert response.status_code == 400
+    assert response.json() == {
+        "status":"400",
+        "message": "Bad request"
+        }
